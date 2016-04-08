177432253340d6e06631311209b00f42c4f39c64
alamatita <alamatita@gmail.com>
2016-04-08 18:39:55 -0300

    pagina de inicio

--- src/main/java/service/HelloWorldService.java
+++ src/main/java/service/HelloWorldService.java
@@ -5,14 +5,21 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/helloworld")
+@Path("/")
 @ApplicationPath("/")
 public class HelloWorldService {
 
     @GET
-    @Produces("text/plain")
+    @Produces("text/html")
     public String getClichedMessage() {
-        return "Hello World";
+    	
+    	String salida = "";
+    	salida += "<!DOCTYPE html><html><head><meta charset=\"UTF-8\">"
+    			+ "<title>Taller 1 TBD</title></head><body>"
+    			+ "La documentación de la API esta en el <a href=\"https://github.com/alamatita/Taller-1-TBD\">repositorio github</a>."
+    			+ "</body></html>";
+
+        return salida;
     }
 	
 }
